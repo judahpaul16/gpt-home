1b60acaf9fb614e6a1e6b80ea6f5b649dd80ff9e
Judah Paul <judahpaul16@gmail.com>
2023-09-09 08:07:21 -0400

    Revert "Debugging Tweaks"
    This reverts commit b941f96d07fe64aaa9c98d6459eb9ef414e664ea.

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ from functions import *
 async def main():
     while True:
         try:
-            ellipses_task = asyncio.create_task(animate_ellipses(display))
             text = await listen_speech(loop, display)
-            ellipses_task.cancel()
             print(f"Heard: {text}")
             await updateLCD(f"Heard: {text}", display)
             # speak(text)
--- functions.py
+++ functions.py
@@ -102,8 +102,3 @@ def query_openai(text):
         stop=["\n", " Human:", " AI:"]
     )
     return response.choices[0].text
-
-async def animate_ellipses(display):
-    for i in range(4):
-        await updateLCD("Listening" + '.' * i, display)
-        await asyncio.sleep(0.5)
