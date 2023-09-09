a1f5ac65dfa10f6bc612c83c0a63f4921b9ca53c
Judah Paul <judahpaul16@gmail.com>
2023-09-09 08:16:11 -0400

    Debugging Debugging

--- app.py
+++ app.py
@@ -5,22 +5,25 @@ from functions import *
 async def main():
     while True:
         try:
+            state_task = asyncio.create_task(display_state("Listening", display))
             text = await listen_speech(loop, display)
+            state_task.cancel()
             print(f"Heard: {text}")
             await updateLCD(f"Heard: {text}", display)
-            # speak(text)
-            # response = query_openai(text)
-            # speak(response)
         except sr.UnknownValueError:
             print("Could not understand audio.")
             asyncio.create_task(updateLCD("Could not understand audio.", display))
+            state_task.cancel()
         except sr.RequestError as e:
             print(f"Could not request results; {e}")
             asyncio.create_task(updateLCD(f"Could not request results; {e}", display))
+            state_task.cancel()
+        except asyncio.CancelledError:
+            pass
         except Exception as e:
             print(f"An error occurred: {traceback.format_exc()}")
             asyncio.create_task(updateLCD(f"An error occurred: {e}", display))
-
+            state_task.cancel()
 
 # Initialize LCD
 display = initLCD()
--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
 from concurrent.futures import ThreadPoolExecutor
-executor = ThreadPoolExecutor()
 import speech_recognition as sr
 from board import SCL, SDA
 import adafruit_ssd1306
@@ -12,13 +11,15 @@ import os
 global r
 r = sr.Recognizer()
 
+executor = ThreadPoolExecutor()
+
 def initLCD():
     # Create the I2C interface.
     i2c = busio.I2C(SCL, SDA)
 
     # Create the SSD1306 OLED class.
     # The first two parameters are the pixel width and pixel height. Change these
-    # to the right size for your display!
+    # to the right size for your display
     display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
     # Alternatively, you can change the I2C address of the device with an addr parameter:
     # display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x31)
@@ -27,9 +28,8 @@ def initLCD():
     display.rotation = 2
 
     # Clear the display. Always call show after changing pixels to make the display
-    # update visible!
+    # update visible
     display.fill(0)
-
     display.show()
 
     # Display IP address
@@ -45,47 +45,42 @@ def initLCD():
     return display
 
 async def updateLCD(text, display):
-    # Clear the section of the display where the text will appear.
     display.fill_rect(0, 10, 128, 22, 0)
-    
     ip_address = subprocess.check_output(["hostname", "-I"]).decode("utf-8").split(" ")[0]
     display.text("IP: " + str(ip_address), 0, 0, 1)
-    
-    if text == 'Listening' or text == 'Interpreting':
-        for i in range(4):
+
+    if len(text) > 42:
+        for i in range(0, len(text) - 21, 21):
             display.fill_rect(0, 10, 128, 22, 0)
-            display.text(text + '.' * i, 0, 20, 1)
+            display.text(text[i:i+21], 0, 10, 1)
+            display.text(text[i+21:i+42], 0, 20, 1)
             display.show()
             await asyncio.sleep(0.5)
+    elif len(text) > 21:
+        display.text(text[:21], 0, 10, 1)
+        display.text(text[21:], 0, 20, 1)
     else:
-        if len(text) > 21:
-            if len(text) > 42:
-                for i in range(0, len(text) - 21, 21):
-                    display.fill_rect(0, 10, 128, 22, 0)
-                    display.text(text[i:i+21], 0, 10, 1)
-                    display.text(text[i+21:i+42], 0, 20, 1)
-                    display.show()
-                    await asyncio.sleep(0.5)
-            else:
-                display.text(text[:21], 0, 10, 1)
-                display.text(text[21:], 0, 20, 1)
-        else:
-            display.text(text, 0, 10, 1)
-        display.show()
-        await asyncio.sleep(5)
+        display.text(text, 0, 10, 1)
+    display.show()
+    await asyncio.sleep(5)
+
 
 async def listen_speech(loop, display):
     global r
-    print("Listening...")
-    await updateLCD("Listening", display)
-
     def recognize_audio():
         with sr.Microphone() as source:
             audio = r.listen(source)
             return r.recognize_google(audio)
-        
     return await loop.run_in_executor(executor, recognize_audio)
 
+async def display_state(state, display):
+    while True:
+        for i in range(4):
+            display.fill_rect(0, 10, 128, 22, 0)
+            display.text(f"{state}" + '.' * i, 0, 20, 1)
+            display.show()
+            await asyncio.sleep(0.5)
+            
 def speak(text):
     os.system(f"espeak '{text}'")
 
