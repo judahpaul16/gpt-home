f0a5b3d64a1c53eb9bf2e410316bcf70e0b77a3a
Judah Paul <judahpaul16@gmail.com>
2023-09-11 00:51:02 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -83,10 +83,13 @@ async def updateLCD(text, display, error=False):
     loop_task = asyncio.create_task(loop_text())
     speaking_task = asyncio.create_task(speak(text))
 
+    tasks = [loop_task, speaking_task]
     if not error:
         query_task = asyncio.create_task(query_openai(text))
+        tasks.append(query_task)
 
-    await asyncio.gather(loop_task, speaking_task, query_task)
+    await asyncio.gather(*tasks)
+    loop_task.cancel()
 
     stop_event.set()
 
@@ -119,7 +122,7 @@ async def query_openai(text):
     try:
         response = openai.Completion.create(
             engine="davinci",
-            prompt=f"Q: {text}\nA: (But add a hint of snark)",
+            prompt=f"Q: {text}\nA: (But add a hint of snark and sarcasm)",
             temperature=0.9,
             max_tokens=64,
             top_p=1,
