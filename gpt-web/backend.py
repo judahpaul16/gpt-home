8b58d5e3f16693bd6251b214bd9ac7eddf3578a4
Judah Paul <judahpaul16@gmail.com>
2023-09-14 22:00:44 -0400

    Update backend.py

--- gpt-web/backend.py
+++ gpt-web/backend.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Request, Response, status
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from pathlib import Path
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="build"), name="static")
+app.mount("/static", StaticFiles(directory="build/static"), name="static")
+app.mount("/favicon.ico", FileResponse(Path("build/favicon.ico")), name="favicon")
 
 @app.get("/")
 def read_root():
-    return FileResponse("build/index.html")
+    return FileResponse(Path("build/index.html"))
