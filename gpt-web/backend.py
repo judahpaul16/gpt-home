fd94ec2af0f5300b5aa0658154f95ba24c5f52a8
Judah Paul <judahpaul16@gmail.com>
2023-09-14 22:31:00 -0400

    Build Paths

--- gpt-web/backend.py
+++ gpt-web/backend.py
@@ -4,14 +4,16 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from pathlib import Path
 
+ROOT_DIRECTORY = Path(__file__).parent
+
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="build/static"), name="static")
+app.mount("/static", StaticFiles(directory=ROOT_DIRECTORY / "build" / "static"), name="static")
 
 @app.get("/favicon.ico")
 def read_favicon():
-    return FileResponse(Path("build/favicon.ico"))
+    return FileResponse(ROOT_DIRECTORY / "build" / "favicon.ico")
 
 @app.get("/")
 def read_root():
-    return FileResponse(Path("build/index.html"))
+    return FileResponse(ROOT_DIRECTORY / "build" / "index.html")
