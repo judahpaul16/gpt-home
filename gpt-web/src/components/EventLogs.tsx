4a9472c2c1e0ddee14145bafd56713b11f13722b
Judah Paul <judahpaul16@gmail.com>
2023-09-18 18:51:03 -0400

    Update Event Log in Realtime

--- gpt-web/src/components/EventLogs.tsx
+++ gpt-web/src/components/EventLogs.tsx
@@ -5,12 +5,23 @@ const EventLogs: React.FC = () => {
 
   useEffect(() => {
     const fetchLogs = async () => {
-      const response = await fetch('/logs', { method: 'POST' });
-      const data = await response.json();
-      setLogs(data.log_data);
+      try {
+        const response = await fetch('/logs', { method: 'POST' });
+        const data = await response.json();
+        setLogs(data.log_data);
+      } catch (error) {
+        console.error('Error fetching logs:', error);
+      }
     };
 
+    // Fetch logs initially
     fetchLogs();
+
+    // Set an interval to fetch logs every 5 seconds
+    const intervalId = setInterval(fetchLogs, 5000);
+
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
