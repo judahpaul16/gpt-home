1396bb7b225196301513fbc68423911781d43640
Judah Paul <judahpaul16@gmail.com>
2023-09-19 13:17:12 -0400

    Debugging Event Log Duplicates

--- gpt-web/src/components/EventLogs.tsx
+++ gpt-web/src/components/EventLogs.tsx
@@ -9,10 +9,10 @@ interface Log {
 
 const EventLogs: React.FC = () => {
   const [logs, setLogs] = useState<Log[]>([]);
+  const [currentLogLength, setCurrentLogLength] = useState<number | null>(null);
   const logContainerRef = useRef<HTMLPreElement>(null);
 
   useEffect(() => {
-    // Fetch all logs initially
     const fetchAllLogs = async () => {
       const response = await fetch('/logs', { method: 'POST' });
       const data = await response.json();
@@ -22,6 +22,7 @@ const EventLogs: React.FC = () => {
         type: log.split(":")[0].toLowerCase(),
       }));
       setLogs(allLogs);
+      setCurrentLogLength(allLogs.length);
     };
 
     fetchAllLogs();
@@ -33,48 +34,44 @@ const EventLogs: React.FC = () => {
         const response = await fetch('/last-log', { method: 'POST' });
         const data = await response.json();
         const lastLog = data.last_log;
+
         const response2 = await fetch('/logs', { method: 'POST' });
         const data2 = await response2.json();
-        const fullLog = data2.log_data.split('\n').map((log: string) => ({
-          content: log,
-          isNew: false,
-          type: log.split(":")[0].toLowerCase(),
-        }));
-    
-        if (lastLog && fullLog && fullLog.length > logs.length) {
+        const fullLog = data2.log_data.split('\n');
+
+        if (lastLog && fullLog.length > (currentLogLength || 0)) {
+          setCurrentLogLength(fullLog.length);
+
           setLogs(prevLogs => [...prevLogs, {
             content: lastLog,
             isNew: true,
             type: lastLog.split(":")[0].toLowerCase(),
           }]);
-    
-          // Remove the 'new' flag after 2 seconds
+
           setTimeout(() => {
             setLogs(prevLogs => prevLogs.map(log => ({ ...log, isNew: false })));
           }, 2000);
         }
-    
-        // Scroll to the bottom
+
         if (logContainerRef.current) {
           logContainerRef.current.scrollTop = logContainerRef.current.scrollHeight;
         }
       } catch (error) {
         console.error('Error fetching last log:', error);
       }
-    };    
-  
-    // Fetch the last log every 2 seconds, only after the initial logs are fetched
+    };
+
     if (logs.length > 0) {
       const intervalId = setInterval(fetchLastLog, 2000);
       return () => clearInterval(intervalId);
     }
-  }, [logs]);  
+  }, [logs, currentLogLength]);
 
   const renderLogs = () => {
     return logs.map((log, index) => {
       const key = `${log.content}-${index}`;
       const classes = [log.isNew ? 'new-entry' : 'old-entry', log.type].join(' ');
-  
+
       return (
         <div className={classes} key={key}>
           {log.content}
