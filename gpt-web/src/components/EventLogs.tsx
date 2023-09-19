951f9e0affa500f84d4637ffe4d5e014c33a71f0
Judah Paul <judahpaul16@gmail.com>
2023-09-18 23:14:10 -0400

    Update EventLogs.tsx

--- gpt-web/src/components/EventLogs.tsx
+++ gpt-web/src/components/EventLogs.tsx
@@ -4,31 +4,32 @@ import '../css/EventLogs.css';
 const EventLogs: React.FC = () => {
   const [logs, setLogs] = useState<Array<{ content: string, isNew: boolean, type: string }>>([]);
   const logContainerRef = useRef<HTMLPreElement>(null);
-  const lastLogIndex = useRef(0); // Store the last log index
-  
+  const lastLogTimestamp = useRef<number>(0);
+
   useEffect(() => {
     const fetchLogs = async () => {
       try {
         const response = await fetch('/logs', { method: 'POST' });
         const data = await response.json();
-        const allLogs = data.log_data.split('\n');
-        
-        const newLogs = allLogs.slice(lastLogIndex.current).map((log: string) => {
-          const type = log.split(":")[0]; // Parse type from log string
+        const logArray = data.log_data.split('\n');
+        const currentTime = Date.now();
+
+        const newLogs = logArray.map((log: string) => {
+          const type = log.split(":")[0]; // extract log type
           return { content: log, isNew: true, type: type.toLowerCase() };
         });
-        
-        // Only update if there are new logs
-        if (newLogs.length > 0) {
-          lastLogIndex.current = allLogs.length; // Update the last log index
-          
+
+        // Only add logs that are new
+        if (currentTime > lastLogTimestamp.current) {
           setLogs(prevLogs => [...prevLogs, ...newLogs]);
-          
+
           // Remove the 'new' flag after 2 seconds
           setTimeout(() => {
             setLogs(prevLogs => prevLogs.map(log => ({ ...log, isNew: false })));
           }, 2000);
         }
+
+        lastLogTimestamp.current = currentTime; // Update the last log timestamp
 
         if (logContainerRef.current) {
           logContainerRef.current.scrollTop = logContainerRef.current.scrollHeight;
@@ -37,12 +38,12 @@ const EventLogs: React.FC = () => {
         console.error('Error fetching logs:', error);
       }
     };
-  
+
     fetchLogs();
     const intervalId = setInterval(fetchLogs, 2000);
     return () => clearInterval(intervalId);
   }, []);
-  
+
   const renderLogs = () => {
     return logs.map((log, index) => {
       const key = `${log.content}-${index}`;
