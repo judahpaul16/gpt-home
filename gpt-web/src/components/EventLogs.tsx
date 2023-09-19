cab998575f36b777b2bd4dd6b6049c6ef5388f2b
Judah Paul <judahpaul16@gmail.com>
2023-09-18 23:06:21 -0400

    Tweaks

--- gpt-web/src/components/EventLogs.tsx
+++ gpt-web/src/components/EventLogs.tsx
@@ -4,20 +4,31 @@ import '../css/EventLogs.css';
 const EventLogs: React.FC = () => {
   const [logs, setLogs] = useState<Array<{ content: string, isNew: boolean, type: string }>>([]);
   const logContainerRef = useRef<HTMLPreElement>(null);
-
+  const lastLogIndex = useRef(0); // Store the last log index
+  
   useEffect(() => {
     const fetchLogs = async () => {
       try {
         const response = await fetch('/logs', { method: 'POST' });
         const data = await response.json();
-        const newLogs = data.log_data.split('\n').map((log: any) => ({ content: log, isNew: true, type: 'info' })); // Dummy type
-
-        setLogs(prevLogs => [...prevLogs, ...newLogs]);
-
-        // Remove the 'new' flag after 2 seconds
-        setTimeout(() => {
-          setLogs(prevLogs => prevLogs.map(log => ({ ...log, isNew: false })));
-        }, 2000);
+        const allLogs = data.log_data.split('\n');
+        
+        const newLogs = allLogs.slice(lastLogIndex.current).map((log: string) => {
+          const type = log.split(":")[0]; // Parse type from log string
+          return { content: log, isNew: true, type: type.toLowerCase() };
+        });
+        
+        // Only update if there are new logs
+        if (newLogs.length > 0) {
+          lastLogIndex.current = allLogs.length; // Update the last log index
+          
+          setLogs(prevLogs => [...prevLogs, ...newLogs]);
+          
+          // Remove the 'new' flag after 2 seconds
+          setTimeout(() => {
+            setLogs(prevLogs => prevLogs.map(log => ({ ...log, isNew: false })));
+          }, 2000);
+        }
 
         if (logContainerRef.current) {
           logContainerRef.current.scrollTop = logContainerRef.current.scrollHeight;
@@ -26,12 +37,12 @@ const EventLogs: React.FC = () => {
         console.error('Error fetching logs:', error);
       }
     };
-
+  
     fetchLogs();
     const intervalId = setInterval(fetchLogs, 2000);
     return () => clearInterval(intervalId);
   }, []);
-
+  
   const renderLogs = () => {
     return logs.map((log, index) => {
       const key = `${log.content}-${index}`;
